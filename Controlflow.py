0bbd35b72a019b5ee10db855ba64dc933d66fde7
Varun Kudalkar <varun.kudalkar@gmail.com>
2019-09-22 11:42:01 +0530

    loops

--- Controlflow.py
+++ Controlflow.py
@@ -34,3 +34,49 @@ else:
     print('a is greater than b')
 
 # Loops
+
+
+'''
+while condition:
+    statements
+    incr/decr
+'''
+# i = 0
+# while i < 10:
+#     print('Codekul')
+#     i += 1
+
+# Sum of all even elements from 0-20
+
+i = 0
+even_sum = 0
+odd_sum = 0
+
+while i <= 20:
+    if i % 2 == 0:
+        even_sum += i
+    else:
+        odd_sum += i
+    i += 1
+
+print("Even Sum:", even_sum)
+print("Odd Sum:", odd_sum)
+
+'''
+for item in collection:
+    statements
+'''
+
+# for ch in "Codekul":
+#     if ch == "C":
+#         print(ch)
+
+l1 = [23,4,78,23,78,35,83]
+# sum = 0
+for no in l1:
+    if no % 2 == 0:
+        print('{} is even'.format(no))
+    else:
+        print('{} is odd'.format(no))
+
+# print('Sum:', sum)
